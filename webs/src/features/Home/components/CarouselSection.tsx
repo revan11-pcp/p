84a96d04278d5ae6a7d0df13f30bad832798eaae
revan11-pcp <muhammad.firdaus@pcpexpress.com>
2025-11-11 09:24:33 +0700

    react icon

--- webs/src/features/Home/components/CarouselSection.tsx
+++ webs/src/features/Home/components/CarouselSection.tsx
@@ -1,15 +1,20 @@
 import { Carousel } from 'flowbite-react';
 import csImg from '../../../assets/cs.png';
+import { HiArrowLeft, HiArrowRight } from 'react-icons/hi';
 
 function CarouselSection() {
+  const images = [csImg, csImg, csImg, csImg, csImg];
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel slideInterval={3000} pauseOnHover>
-        <img src={csImg} alt="Slide 1" />
-        <img src={csImg} alt="Slide 2" />
-        <img src={csImg} alt="Slide 3" />
-        <img src={csImg} alt="Slide 4" />
-        <img src={csImg} alt="Slide 5" />
+      <Carousel 
+        slideInterval={3000} 
+        pauseOnHover
+        leftControl={<HiArrowLeft className="h-6 w-6 text-white" />}
+        rightControl={<HiArrowRight className="h-6 w-6 text-white" />}
+      >
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`Slide ${index + 1}`} />
+        ))}
       </Carousel>
     </div>
   );
