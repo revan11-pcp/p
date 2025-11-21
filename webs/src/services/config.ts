882c82c84c9d399f6964d1b20391124c271306ec
revan11-pcp <muhammad.firdaus@pcpexpress.com>
2025-11-21 09:50:55 +0700

    feat

--- webs/src/services/config.ts
+++ webs/src/services/config.ts
@@ -1,8 +1,9 @@
 const getApiBaseUrl = () => {
-  if (import.meta.env.DEV && import.meta.env.VITE_API_PROXY_TARGET) {
-    return import.meta.env.VITE_API_PROXY_TARGET;
+  if (import.meta.env.DEV) {
+    return import.meta.env.VITE_API_PROXY_TARGET; // "/api"
   }
-  return import.meta.env.VITE_API_BASE_URL;
+
+  return import.meta.env.VITE_API_BASE_URL; // "https://pcptransport.com/api"
 }
 
 export const API_BASE_URL = getApiBaseUrl();
