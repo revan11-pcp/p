b126a9df6b5c0d3a7a69137113ddbe84ede5c9b4
revan11-pcp <muhammad.firdaus@pcpexpress.com>
2025-11-21 08:47:14 +0700

    feat

--- webs/src/services/config.ts
+++ webs/src/services/config.ts
@@ -1,9 +1,7 @@
 const getApiBaseUrl = () => {
-  // In development, use the proxy target if it exists.
   if (import.meta.env.DEV && import.meta.env.VITE_API_PROXY_TARGET) {
     return import.meta.env.VITE_API_PROXY_TARGET;
   }
-  // In production, or if no proxy target is set, use the base URL.
   return import.meta.env.VITE_API_BASE_URL;
 }
 
